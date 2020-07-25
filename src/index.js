3a54717be4c4a8b6fd6039d5b635e408088e4e22
Sokoya Philip <philo4u2c@gmail.com>
2020-07-25 05:21:03 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-//@ts-check
 var ip = require("ip");
 const express = require('express')
 require('express-async-errors');
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-
-const app = express()
+const PORT= process.env.PORT || '8080' 
+const app = express();
+app.set("port", PORT);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(morgan('tiny'))
+
+const https = require('https');
 
 /**
  * @type {Array<{
@@ -22,13 +24,21 @@ app.use(morgan('tiny'))
  *  }>}
  */
 const db = []
-
 const getLastId = () => db.length ? db[db.length-1].id : 0
 const [PENDING, DONE] = [0, 1]
 
+// filter to get the result...
 app.get('/ussd-requests', (req, res) => {
-  const { status, type } = req.query
-
+  const { id, status, type } = req.query
+  if(id){
+    var result  = "Approved Manually";
+    for(let i in db) {
+        if(db[i].id.toString() === id) {
+          db[i] = { ...db[i], result, status: DONE }
+          return res.send(db[i])
+        }
+    }
+  }
   const data = (() => {
     if(type && status) {
       return db
@@ -45,39 +55,85 @@ app.get('/ussd-requests', (req, res) => {
   res.send(data)
 })
 
+
+// Get single transaction detail.
+app.get('/get-transaction/', (req, res) => {
+  const { id } = req.query
+  if(id){
+    for(let i in db) {
+        if(db[i].id.toString() === id) {
+          return res.send(db[i])
+        }
+    }
+  }
+})
+
+
+// post - Add a new Item from your API
 app.post('/ussd-requests', (req, res) => {
-  const { code, choices, type } = req.body
-
-  const newUssdRequest = { id: getLastId()+1, code, choices, type, status: PENDING }
+  const {  id, code, choices, type } = req.body
+  const newUssdRequest = { id, code, choices, type, status: PENDING }
   db.push(newUssdRequest)
-
   res.send({ ...newUssdRequest })
 })
 
+
+// update the heroku app base on the result got from the ussd worker.
 app.put('/ussd-requests/:id', (req, res) => {
+
   const { result } = req.body
   const id = req.params.id
-
   for(let i in db) {
     if(db[i].id.toString() === id) {
       db[i] = { ...db[i], result, status: DONE }
-      return res.send(db[i])
+      // webhook...
+      const postData = JSON.stringify({'trans_id': req.body.id, 'remark': req.body.result})
+      const options = {
+        hostname: 'www.payscribe.ng',
+        path: '/webhook/ussd_response',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+      };
+      // Using node Js request, version 2 should use postman-request package.
+      const webbookRequest = https.request(options, (webhookResponse) => {
+        webhookResponse.setEncoding('utf8');
+        webhookResponse.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        webhookResponse.on('end', () => {
+            console.log('No more data in response.');
+            // finally log to db
+            return res.send(db[i])
+        });
+      });
+      webbookRequest.on('error', (e) => {
+        console.error(`problem with webhook request: ${e.message}`);
+      });
+      // Write data to request body
+      webbookRequest.write(postData);
+      webbookRequest.end();
     }
   }
-
 })
 
+
 app.use((err, req, res, next) => {
-  if (res.headersSent) { return next(err) }
+  
+  if (res.headersSent) { 
+    return next(err) 
+  }
   res.status(500)
   res.json({ message: err.message })
 })
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Server started at port ${port}`)
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`)
   console.log('Available on:')
-  console.log(` • http://localhost:${port}`)
-  console.log(` • http://${ip.address()}:${port}`)
+  console.log(` • http://localhost:${PORT}`)
+  console.log(` • http://${ip.address()}:${PORT}`)
   console.log('\n')
 })
