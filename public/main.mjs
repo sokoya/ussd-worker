540e2871f88f46cb80b9e47ae6bb1947bc969cf9
sokoya <philo4u2c@gmail.com>
2020-08-07 15:04:01 +0100

    commit

--- public/main.mjs
+++ public/main.mjs
@@ -3,17 +3,21 @@ import { http } from './utils.mjs'
 const app = new Vue({
   el: '#app',
   data: {
-    newUssdRequest: '',
-    newUssdRequestType: '',
+    newUssdRequest: '', // The Choices (USSD)
+    newUssdRequestType: '', // type either SIM 1 or 2 ( 1 or 2)
     ussdRequests: []
   },
   methods: {
     addUssdRequest: async function() {
+      const id = Math.random()
+      // check if the type and message body is not empty
       if(!this.newUssdRequest || !this.newUssdRequestType) { return }
 
-      const [code, ...choices] = this.newUssdRequest.split('-').map(s => s.trim())
+      // where code is *123# and choices can be *1, *4 ... with  -
+      // const [code, ...choices] = this.newUssdRequest.split('-').map(s => s.trim())
+      const [code, ...choices] = this.newUssdRequest.split('-').map(s => s.trim().toString())
       const type = this.newUssdRequestType
-      const ussdRequest = await http.post('/ussd-requests', { code, choices, type })
+      const ussdRequest = await http.post('/ussd-requests', { id, code, choices, type })
 
       this.ussdRequests.push(ussdRequest)
       this.newUssdRequest = ''
